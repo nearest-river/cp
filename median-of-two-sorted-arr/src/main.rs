1a3db0c646cf00d3fffefc06d91f8121f3939bf1
Nate River <trishiragolder69@outlook.com>
2024-07-17 05:14:45 +0530

    implemented median-of-two-sorted-arr

--- median-of-two-sorted-arr/src/main.rs
+++ median-of-two-sorted-arr/src/main.rs
@@ -1,3 +1,26 @@
+
+
+pub struct Solution;
+impl Solution {
+  pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+    let mut nums=Vec::<i32>::with_capacity(nums2.len()+nums2.len());
+    nums.extend_from_slice(&nums1);
+    nums.extend_from_slice(&nums2);
+
+    nums.sort();
+
+    match nums.len()&1 {
+      0=> {
+        let i=nums.len()/2;
+        (nums[i]+nums[i-1]) as f64/2.
+      },
+      _=> nums[nums.len()/2] as _
+    }
+  }
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+  
 }
